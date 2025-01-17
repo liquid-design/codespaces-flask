12a42ca94840d17355903171691b02bd6a10145a
Sander Schouten <info@liquid-design.be>
2025-01-17 14:00:59 +0000

    Read Data from CSV Files

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -8,7 +9,9 @@ def home():
 
 @app.route("/timeline")
 def timeline():
-    return render_template("timeline.html", title="Timeline")
+    data = pd.read_csv('historical_events.csv')
+    events = data.to_dict(orient='records')
+    return render_template("timeline.html", title="Timeline", events=events)
 
 if __name__ == '__main__':
     app.run(debug=True)
